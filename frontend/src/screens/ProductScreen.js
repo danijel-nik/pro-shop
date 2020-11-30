4d3f912bcdce9935e123c71fc01507ce04362be8
Danijel Nikolic <danijel_nikolic@netex.ro>
2020-12-01 00:54:38 +0200

    backend configuration

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = ({ match }) => {
-    const product = products.find(p => p._id === match.params.id)
+
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [match])
+
+    // const product = products.find(p => p._id === match.params.id)
 
     return (
         <div>
             <Row className='my-3'>
                 <Col>
-                    <Link className='' to='/'>Go Back</Link>
+                    <Link to='/'>Go Back</Link>
                 </Col>
             </Row>
             
