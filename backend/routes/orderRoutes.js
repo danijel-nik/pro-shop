e356987f9dc503502ab93404c7ef9aabcc74200d
danijel_nikolic <danijel_nikolic@netex.ro>
2020-12-21 09:59:15 +0100

    get order by ID controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { addOrderItems } = require('../controllers/orderController')
+const { addOrderItems, getOrderByID } = require('../controllers/orderController')
 const { protect } = require('../middleware/authMiddleware')
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 router.route('/').post(protect, addOrderItems)
+
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+router.route('/:id').get(protect, getOrderByID)
 
 module.exports = router
