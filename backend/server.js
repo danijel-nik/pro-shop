2d4f55cc1b5c9c820937cae3f8c180484ffa5c5d
danijel_nik <danijel.nikolic@ymail.com>
2021-01-23 22:20:15 +0100

    Image upload endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
@@ -6,6 +7,7 @@ const connectDB = require('./config/db')
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes')
+const uploadRoutes = require('./routes/uploadRoutes')
 // Middlewares
 const errorMiddleware = require('./middleware/errorMiddleware')
 
@@ -27,9 +29,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 // Get PayPal client ID
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+// const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // Custom middlewares
 // Error handling
